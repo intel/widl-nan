bc38e65343dbaed712f1dba8568987043c99ecd6
kenny-y <kaining.yuan@intel.com>
2016-08-26 21:03:53 +0800

    Adding support to a dictionary containing a member with type is enum

---
+++ test/dictionary-enum/addon.cpp
@@ -0,0 +1,10 @@
+// Add your copyright and license header
+
+#include "gen/nan__computing_device_network.h"
+
+void initModule(v8::Local<v8::Object> exports) {
+  NanComputingDeviceNetwork::Init(exports);
+
+}
+
+NODE_MODULE(testAttributes, initModule);
---
+++ test/dictionary-enum/computing_device_network.cpp
@@ -0,0 +1,34 @@
+// To add your copyright and license header
+
+#include "computing_device_network.h"
+
+ComputingDeviceNetwork::ComputingDeviceNetwork() {
+  // TODO(widl-nan): init your members
+}
+
+ComputingDeviceNetwork::~ComputingDeviceNetwork() {
+  // TODO(widl-nan): do cleanup if necessary
+}
+
+ComputingDeviceNetwork& ComputingDeviceNetwork::operator = (const ComputingDeviceNetwork& rhs) {
+  if (&rhs != this) {
+    // TODO(widl-nan): copy members from rhs
+  }
+  return *this;
+}
+
+void ComputingDeviceNetwork::addNode(const NodeAttr& node) {
+  // TODO(widl-nan): fill your code here
+  name_ = node.get_name();
+  quantity_ = node.get_quantity();
+  type_ = node.get_type();
+}
+
+NodeAttr ComputingDeviceNetwork::getLastNode() {
+  // TODO(widl-nan): fill your code here
+  NodeAttr attr;
+  attr.set_name(name_);
+  attr.set_quantity(quantity_);
+  attr.set_type(type_);
+  return attr;
+}
